d8ad878bc3fc50e226f4f7ff25986293de738cf1
fedegott <federicoluisgottardo@gmail.com>
2018-02-13 20:34:00 +0000

    Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//using arg, to add them go to tools>edit configurations> "program arguments"
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
